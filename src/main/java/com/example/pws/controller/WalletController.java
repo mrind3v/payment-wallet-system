dacc0f2716880bb444bbb3224e739706f774215c
mrind3v <mrinmaydev@outlook.com>
2026-01-31 22:36:25 +0530

    added missing imports

--- src/main/java/com/example/pws/controller/WalletController.java
+++ src/main/java/com/example/pws/controller/WalletController.java
@@ -1,4 +1,6 @@
 package com.example.pws.controller;
+
+import com.example.pws.dto.GetBalanceResponse;
 import com.example.pws.model.Wallet;
 import com.example.pws.service.WalletService;
 import org.springframework.http.ResponseEntity;
@@ -6,10 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
-import java.math.BigDecimal;
-
 @RestController
-@RequestMapping("/api/{id}/wallet" )
+@RequestMapping("/api/{id}/wallet")
 public class WalletController {
 
     private final WalletService walletService;
@@ -19,17 +19,16 @@ public class WalletController {
     }
 
     @GetMapping("/{walletId}")
-    public ResponseEntity<Wallet > getWalletById(@PathVariable String walletId) {
+    public ResponseEntity<Wallet> getWalletById(@PathVariable String walletId) {
         Wallet response = walletService.getWalletById(walletId);
-        return ResponseEntity.ok(response) ;
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/balance" )
-    public GetBalanceResponse getBalanceById(@PathVariable String id )
-    {
-        GetBalanceResponse rs= new GetBalanceResponse() ;
-        BigDecimal balance= walletService.getBalanceById(id ) ;
-        rs.setBalance(balance ) ;
-        return rs ;
+    @GetMapping("/balance")
+    public GetBalanceResponse getBalanceById(@PathVariable String id) {
+        GetBalanceResponse rs = new GetBalanceResponse();
+        BigDecimal balance = walletService.getBalanceById(id);
+        rs.setBalance(balance);
+        return rs;
     }
 }
